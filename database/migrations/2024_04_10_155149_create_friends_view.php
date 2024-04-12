cbbdf04cf86d86764c97b50650dcd2adb8999e18
Dominik <d.jasinski@ideo.pl>
2024-04-12 17:57:52 +0200

    feat: temporary comment

--- database/migrations/2024_04_10_155149_create_friends_view.php
+++ database/migrations/2024_04_10_155149_create_friends_view.php
@@ -3,7 +3,7 @@
 use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+use Staudenmeir\LaravelMergedRelations\Facades\Schema;
 
 return new class extends Migration
 {
@@ -12,10 +12,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::createMergeView(
-            'friends_view',
-            [(new User())->acceptedFriendsTo(), (new User())->acceptedFriendsFrom()]
-        );
+//        Schema::createMergeView(
+//            'friends_view',
+//            [(new User())->acceptedFriendsTo(), (new User())->acceptedFriendsFrom()]
+//        );
     }
 
     /**
@@ -23,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('friends_view');
+        Schema::dropView('friends_view');
     }
 };
