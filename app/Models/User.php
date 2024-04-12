d24484204c3d119f9648e047f8bb0dc75ce5eedb
Dominik <d.jasinski@ideo.pl>
2024-04-12 17:57:40 +0200

    fix: add missing import

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,7 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
-
+use Staudenmeir\LaravelMergedRelations\Eloquent\HasMergedRelationships;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
