9c2f71df89a971a409a0518d1706895d097d6d9b
Dominik <d.jasinski@ideo.pl>
2024-05-21 14:14:02 +0200

    feat: add icons

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,11 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+import { faCircleInfo, faStar, faPaperPlane, faPaperclip, faSmile } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faCircleInfo, faStar, faPaperPlane, faPaperclip, faSmile)
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
