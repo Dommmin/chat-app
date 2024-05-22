9a1d5c642454f953c7f4d3b9072f186654fed86d
Dominik <d.jasinski@ideo.pl>
2024-05-22 10:16:25 +0200

    fix: coding style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,19 @@
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
-import daisyui from "daisyui";
+import daisyui from 'daisyui';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './vendor/laravel/jetstream/**/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.vue',
-    ],
-    daisyui: {
-        themes: ["emerald", "night"],
-    },
+   content: [
+      './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+      './vendor/laravel/jetstream/**/*.blade.php',
+      './storage/framework/views/*.php',
+      './resources/views/**/*.blade.php',
+      './resources/js/**/*.vue',
+   ],
+   daisyui: {
+      themes: ['emerald', 'night'],
+   },
 
-    plugins: [forms, typography, daisyui],
+   plugins: [forms, typography, daisyui],
 };
